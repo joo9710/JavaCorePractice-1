dc3ec2318403556807f57fb84b019e34a6b58492
gonnichiwa <keepthefaith00@gmail.com>
2021-08-06 13:39:21 +0900

    Interface (arraylist의 예를 들어) 설명 코드

---
+++ InterfacePractice/src/kr/ac/dgd/Main.java
@@ -0,0 +1,29 @@
+package kr.ac.dgd;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // interface란 뭔가? : 서로 다른 객체에 구현된 공통된 기능을 쓸 수 있도록 한다.
+        
+        List<String> arrayList = new ArrayList<String>();
+        List<String> linkedList = new LinkedList<String>();
+
+        // arrayList에 string 데이터를 넣는다.
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+        // linkedList에 string 데이터를 넣는다.
+        linkedList.add("1");
+        linkedList.add("2");
+        linkedList.add("3");
+
+
+
+
+
+    }
+}
